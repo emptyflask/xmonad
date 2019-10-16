2bbab7b198da8fc0c32ce44a486e93744588413a
Jon Roberts <jon@emptyflask.net>
2019-10-15 23:49:34 -0500

    layouts and colors

--- lib/Layout.hs
+++ lib/Layout.hs
@@ -6,9 +6,9 @@ module Layout (myLayoutHook) where
 import XMonad.Layout
 import XMonad.Layout.BinarySpacePartition
 import XMonad.Layout.BorderResize
-import XMonad.Layout.Circle
 -- import XMonad.Layout.ComboP
 -- import XMonad.Layout.Grid
+import XMonad.Layout.Column
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Reflect
 import XMonad.Layout.Renamed (renamed, Rename(Replace))
@@ -16,18 +16,34 @@ import XMonad.Layout.ResizableTile
 import XMonad.Layout.Spacing
 import XMonad.Layout.Tabbed
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.ShowWName
 -- import XMonad.Layout.TwoPane
 
-myLayoutHook = ntile ||| rtile ||| mtile ||| full ||| tab ||| threeCol ||| bsp ||| Circle
+import qualified Colors as C
+
+myLayoutHook = showWName $ tallLeft ||| tallRight ||| full ||| threeCol ||| bsp ||| tab
   where
-    rt      = defaultSpacing 4 $ ResizableTall 1 (2/100) (8/13) []
-    -- normal vertical tile
-    ntile   = named "[]="   $ smartBorders rt
-    rtile   = named "=[]"   $ reflectHoriz $ smartBorders rt
+    tallLeft   = named "[]="   rt
+    tallRight  = named "=[]"   $ reflectHoriz rt
+    full       = named "[ ]"   $ noBorders Full
+    one        = named " o "   $ Column 1.6
+    threeCol   = named "|||"   $ withBorders $ ThreeColMid 1 delta (1/2)
+    bsp        = named "BSP"   $ withBorders $ borderResize emptyBSP
+
+    rt = withBorders $ ResizableTall 1 delta goldenRatio []
+    delta = 3/100
+    goldenRatio = 0.618033
+    withBorders = smartBorders . defaultSpacing 20
+    defaultSpacing i = spacingRaw True (uniformBorder i) True (uniformBorder i) True
+    uniformBorder i = Border i i i i
+    named t = renamed [Replace t]
+
     -- normal horizontal tile
-    mtile   = named "M[]="  $ smartBorders $ Mirror rt
+    -- mtile   = named "M[]="  $ smartBorders $ Mirror rt
+
     -- fullscreen with tabs
     tab     = named "T"     $ noBorders $ tabbed shrinkText tabTheme
+
     -- two tab panes beside each other, move windows with SwapWindow
     -- tabB    = noBorders     $ tabbed shrinkText tabTheme
     -- tabtile = named "TT"    $ combineTwoP (TwoPane 0.03 0.5)
@@ -42,28 +58,20 @@ myLayoutHook = ntile ||| rtile ||| mtile ||| full ||| tab ||| threeCol ||| bsp |
     --                                                             (Role "gimp-dock")
     --                                              )
     --                                       (Role "gimp-toolbox")
-    -- fullscreen without tabs
-    full    = named "[]"    $ noBorders Full
-
-    threeCol = withBorders $ ThreeColMid 1 (2/100) (1/2)
-
-    bsp = withBorders $ borderResize emptyBSP
-
-    withBorders = smartBorders . defaultSpacing 4
-    defaultSpacing i = spacingRaw True (uniformBorder i) True (uniformBorder i) True
-    uniformBorder i = Border i i i i
-
-    -- make renamed work more like named, which is now deprecated
-    named t = renamed [Replace t]
 
 tabTheme = def
-    { activeColor           = "white"
-    , inactiveColor         = "grey"
-    , urgentColor           = "red"
-    , activeBorderColor     = "grey"
-    , inactiveBorderColor   = "grey"
-    , activeTextColor       = "black"
-    , inactiveTextColor     = "black"
+    { activeColor           = C.bg4
+    , activeBorderColor     = C.bg0
+    , activeTextColor       = C.fg0
+
+    , inactiveColor         = C.bg1
+    , inactiveBorderColor   = C.bg0
+    , inactiveTextColor     = C.fg1
+
+    , urgentColor           = C.red
+    , urgentBorderColor     = C.bg1
+    , urgentTextColor       = C.bg0
+
     , decoHeight            = 22
     , fontName              = "xft:Liberation Sans:size=10"
     }
